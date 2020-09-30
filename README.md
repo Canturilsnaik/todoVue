fb4512973f45b69346d8a8c3488079687cb7635f
Canturilsnaik <italo.cantur@gmail.com>
2020-09-30 01:34:23 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todo List with VueJS
+
+ To do list made with VueJS, for learn
---
+++ index.js
@@ -0,0 +1,20 @@
+var todo = new Vue({
+  el: '.todo',
+
+  data: {
+    field: '',
+    item: []
+  },
+
+  methods: {
+    addItem: function() {
+      if(!this.field) return 
+      
+      this.item.unshift(this.field)
+    },
+
+    removeItem: function(){
+      this.item.splice(this.item, 1)
+    }
+  }
+})
